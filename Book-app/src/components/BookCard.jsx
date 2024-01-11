181b4ada0090bd2f6625b129bd68105c63ec22dc
parisa abasi <parisaabasi212@gmail.com>
2024-01-12 00:29:01 +0330

    add favorit book's box

--- Book-app/src/components/BookCard.jsx
+++ Book-app/src/components/BookCard.jsx
@@ -4,11 +4,12 @@ import { GoHeartFill } from "react-icons/go";
 import styles from './BookCard.module.css';
 
 
-function BookCard({data}) {
-    const {id , title, author,image,pages,language} =data;
+function BookCard({data ,likedBoolHandler}) {
+    const {title, author,image,pages,language} =data;
     const[like, setLike]=useState(false);
 
     const likeHandler=()=>{
+      likedBoolHandler(data,like)
       setLike(like => !like);
     }
 
@@ -22,7 +23,7 @@ function BookCard({data}) {
             <span>{language}</span><span>{pages} pages</span>
           </div>
         </div>
-        <button onClick={likeHandler}><GoHeartFill font-size="1.5rem" color={like?"red":"#fff"}/></button>
+        <button onClick={likeHandler}><GoHeartFill fontSize="1.5rem" color={like?"red":"#fff"}/></button>
 
     </div>
   )
--- Book-app/src/components/Books.jsx
+++ Book-app/src/components/Books.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { books } from "../constants/mockData";
 import BookCard from './BookCard';
+import SideCard from './SideCard';
+
+import styles from "./Books.module.css";
 
 function Books() {
+  const [likedBooks , setLikedBooks] = useState([]);
+
+  const likedBoolHandler =(book,status)=>{
+      if(status){
+        const newList=likedBooks.filter(i=>i.id !== book.id);
+        setLikedBooks(newList);
+      }else{
+        setLikedBooks(likedBooks =>[...likedBooks, book]);
+      }
+  }
   return (
-    <div>
-        <div>
-            {books.map((book) => <BookCard key={book.id} data={book}/>)}
+    <div className={styles.container}>
+        <div className={styles.bookList}>
+            {books.map((book) => <BookCard key={book.id} data={book} likedBoolHandler={likedBoolHandler}/>)}
         </div>
-        <div></div>
+        
+        {!!likedBooks.length && 
+        <div className={styles.favorite}>
+          <h1>Favorites</h1>
+          {likedBooks.map(book=><SideCard key={book.id} data={book}/>)}</div>}
     </div>
   )
 }
